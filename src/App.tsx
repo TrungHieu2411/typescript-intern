835033c514e9b818ab92f8e423da081fbc1794a8
hieutlt <121414290+TrungHieu2411@users.noreply.github.com>
2023-07-20 17:59:25 +0700

    thay đổi firestore

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,23 @@ import "firebase/compat/auth";
 import "firebase/compat/storage";
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyDWmnQxW7q8r_OC5dxiBkueulVYGl0b-Ro",
-//   authDomain: "queuing-system-b3bad.firebaseapp.com",
-//   projectId: "queuing-system-b3bad",
-//   storageBucket: "queuing-system-b3bad.appspot.com",
-//   messagingSenderId: "743235220310",
-//   appId: "1:743235220310:web:a76c721afdb5aa781b9cf7",
-//   measurementId: "G-G54M2M7CCD",
+//   apiKey: "AIzaSyC-W2yXlCkE_gdvGQJPTfxx11Mwi25_v2E",
+//   authDomain: "qeuingsystem.firebaseapp.com",
+//   projectId: "qeuingsystem",
+//   storageBucket: "qeuingsystem.appspot.com",
+//   messagingSenderId: "376592044161",
+//   appId: "1:376592044161:web:f293f8c7aa06c3bce8bd80",
+//   measurementId: "G-35DPS4BG70"
 // };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-W2yXlCkE_gdvGQJPTfxx11Mwi25_v2E",
-  authDomain: "qeuingsystem.firebaseapp.com",
-  projectId: "qeuingsystem",
-  storageBucket: "qeuingsystem.appspot.com",
-  messagingSenderId: "376592044161",
-  appId: "1:376592044161:web:f293f8c7aa06c3bce8bd80",
-  measurementId: "G-35DPS4BG70"
+  apiKey: "AIzaSyAGwEzRPXgUH1BHHzI9n7_sddyf_R5ARuM",
+  authDomain: "qeuingsystem-14000.firebaseapp.com",
+  projectId: "qeuingsystem-14000",
+  storageBucket: "qeuingsystem-14000.appspot.com",
+  messagingSenderId: "510878111082",
+  appId: "1:510878111082:web:fd2cce60dd86946a138113",
+  measurementId: "G-S4VB9VDYH1"
 };
 
 // Initialize Firebase
